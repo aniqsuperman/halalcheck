016b3ab0e49ab10dcdc6af397701201eab30f3d6
aniqsuperman <aniqsabir0@gmail.com>
2025-05-20 14:25:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify, send_from_directory, render_template
 import requests
 from flask_cors import CORS
-
-
-
+import os
 
 
 app = Flask(__name__)
+
+
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 @app.route('/')
 def index():
